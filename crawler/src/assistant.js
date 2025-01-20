9cdd8f0d58a67b9278d0139d27fcf882689d4ec2
saulopaiva <saulo.paiva@squadra.com.br>
2025-01-20 20:47:15 -0300

    refact: separete assistant.js from crawler index

---
+++ crawler/src/assistant.js
@@ -0,0 +1,61 @@
+
+import { ChatOpenAI } from '@langchain/openai';
+import { createRetrieverTool } from "langchain/tools/retriever";
+import { AgentExecutor, createOpenAIFunctionsAgent } from "langchain/agents";
+import { ChatPromptTemplate, MessagesPlaceholder } from "@langchain/core/prompts";
+
+import vectorStore from './vector-store/memory-vector-store.js';
+
+//// ############################
+//// ASSISTANT SETUP
+
+/**
+ * Create a retriever tool from the vector store
+ *
+ * The retriever tool is a tool that allows the agent to search for documents in the vector store
+ * based on the user input.
+ *
+ * Parameters:
+ * - k: the number of documents to retrieve
+ * - searchType: the type of search to perform (mmr, bm25, etc)
+ * - verbose: whether to print debug information
+ * - searchKwargs: the search parameters (fetchK, lambda, etc)
+ *    - fetchK: an intermediate number of results to fetch before applying further filtering or ranking.
+ *    - lambda: the trade-off between relevance and diversity. A value of 0.5 suggests an equal emphasis on both aspects.
+ */
+const retriever = vectorStore.asRetriever({
+  k: 10,
+  searchType: 'mmr',
+  verbose: false,
+  searchKwargs: {
+    fetchK: 40,
+    lambda: 0.5
+  },
+});
+
+const tool = createRetrieverTool(retriever, {
+  name: 'search_latest_knowledge',
+});
+
+const chatModel = new ChatOpenAI({ model: process.env.MODEL_CHAT });
+
+const prompt = ChatPromptTemplate.fromMessages([
+  ['system', process.env.AGENT_SYSTEM_TEMPLATE],
+  // new MessagesPlaceholder('chat_history'),
+  ['human', '{input}'],
+  new MessagesPlaceholder('agent_scratchpad'),
+]);
+
+const agent = await createOpenAIFunctionsAgent({
+  llm: chatModel,
+  tools: [tool],
+  prompt,
+});
+
+const agentExecutor = new AgentExecutor({
+  agent,
+  tools: [tool],
+  returnIntermediateSteps: false,
+});
+
+export default agentExecutor;
--- crawler/src/index.js
+++ crawler/src/index.js
@@ -3,15 +3,12 @@ import readline from 'node:readline';
 import chalk from 'chalk';
 
 // Langchain imports
-import { ChatOpenAI } from '@langchain/openai';
 import { Document } from "langchain/document";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { createRetrieverTool } from "langchain/tools/retriever";
-import { AgentExecutor, createOpenAIFunctionsAgent } from "langchain/agents";
-import { ChatPromptTemplate, MessagesPlaceholder } from "@langchain/core/prompts";
 
 // Custom imports
 import crawler from './crawler.js';
+import agentExecutor from './assistant.js';
 import vectorStore from './vector-store/memory-vector-store.js';
 // import vectorStore from './vector-store/chroma-vector-store.js';
 
@@ -23,11 +20,9 @@ const allowedPagesToVisit = process.env.SITE_ALLOWED_PAGES !== '' ? process.env.
 const maxCrawlLength = process.env.MAX_CRAWL_NUM_PAGES || 10;
 const maxConcurrency = process.env.MAX_CONCURRENT_REQUESTS || 2;
 
-
 const cleanHtml = (html) => {
-  return html.replace(/<script.*>.*<\/script>/ims, '').replace(/<style.*>.*<\/style>/ims, '').replace(/<img[^>]*>/g,'').replace(/<(.|\n)*?>/ig, ' ').replace(/  /ig, '').replace(/\n/g,'|');
+  return html.replace(/<script.*>.*<\/script>/ims, '').replace(/<style.*>.*<\/style>/ims, '').replace(/<img[^>]*>/g,'').replace(/<(.|\n)*?>/ig, ' ').replace(/  /ig, '').replace(/__/ig, '').replace(/\n/g,'|').replace(/\|/g,'');
 }
-
 
 // ############################
 // CRAWLER
@@ -57,59 +52,6 @@ await crawler({
   maxCrawlLength,
   maxConcurrency,
   dataHandler: turnPageIntoVector
-});
-
-
-//// ############################
-//// ASSISTANT SETUP
-
-/**
- * Create a retriever tool from the vector store
- *
- * The retriever tool is a tool that allows the agent to search for documents in the vector store
- * based on the user input.
- *
- * Parameters:
- * - k: the number of documents to retrieve
- * - searchType: the type of search to perform (mmr, bm25, etc)
- * - verbose: whether to print debug information
- * - searchKwargs: the search parameters (fetchK, lambda, etc)
- *    - fetchK: an intermediate number of results to fetch before applying further filtering or ranking.
- *    - lambda: the trade-off between relevance and diversity. A value of 0.5 suggests an equal emphasis on both aspects.
- */
-const retriever = vectorStore.asRetriever({
-  k: 10,
-  searchType: 'mmr',
-  verbose: false,
-  searchKwargs: {
-    fetchK: 40,
-    lambda: 0.5
-  },
-});
-
-const tool = createRetrieverTool(retriever, {
-  name: 'search_latest_knowledge',
-});
-
-const chatModel = new ChatOpenAI({ model: process.env.MODEL_CHAT });
-
-const prompt = ChatPromptTemplate.fromMessages([
-  ['system', process.env.AGENT_SYSTEM_TEMPLATE],
-  // new MessagesPlaceholder('chat_history'),
-  ['human', '{input}'],
-  new MessagesPlaceholder('agent_scratchpad'),
-]);
-
-const agent = await createOpenAIFunctionsAgent({
-  llm: chatModel,
-  tools: [tool],
-  prompt,
-});
-
-const agentExecutor = new AgentExecutor({
-  agent,
-  tools: [tool],
-  returnIntermediateSteps: false,
 });
 
 
